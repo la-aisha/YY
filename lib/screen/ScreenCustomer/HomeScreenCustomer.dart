f29058b858e7bf80d16b9cef8a3b498febcd9aac
la-aisha <sulamaish4738@gmail.com>
2023-10-26 16:02:57 +0200

    second commit

--- lib/screen/ScreenCustomer/HomeScreenCustomer.dart
+++ lib/screen/ScreenCustomer/HomeScreenCustomer.dart
@@ -1,11 +1,12 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:carousel_slider/carousel_controller.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:yy/provider/auth_provider.dart';
 
 class Homescreencustomer extends StatefulWidget {
-  const Homescreencustomer({super.key});
+  const Homescreencustomer({Key? key}) : super(key: key);
 
   @override
   State<Homescreencustomer> createState() => _HomescreencustomerState();
@@ -14,165 +15,206 @@ class Homescreencustomer extends StatefulWidget {
 class _HomescreencustomerState extends State<Homescreencustomer> {
   @override
   Widget build(BuildContext context) {
-    //final user = FirebaseAuth.instance.currentUser;
     final ap = Provider.of<AuthProvider>(context, listen: false);
-    /* return Container(
-      color: Colors.blue,
-      child : Column(
-        children: [
-          Text(ap.userModel.firstname),
-         Text(ap.userModel.lastname),
-          Text(ap.userModel.phoneNumber),
+    var size = MediaQuery.of(context).size;
+    var width = size.width;
+    var height = size.height;
+     Color myColor1 = Color.fromRGBO(40, 0, 81, 1);
+    Color myColor2 = Color.fromRGBO(189, 22, 22, 1);
 
-
-
-        ],
-      )
-    ); */
+    List imageList = [
+      {"id": 1, "image_path": 'images/1.png'},
+      {"id": 2, "image_path": 'images/2.png'},
+      {"id": 3, "image_path": 'images/3.png'}
+    ];
+    final CarouselController carouselController = CarouselController();
+    int currentIndex = 0;
     return Scaffold(
       body: Stack(
         children: [
-           Container(
-         height: double.infinity,
-          width: double.infinity,
-          decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            colors: [
-              Color.fromRGBO(40, 0, 81, 1),
-             //Color.fromRGBO(115, 51, 100, 1),
-              Color.fromRGBO(115, 51, 100, 1),
-              Color.fromRGBO(115, 51, 100, 1),
-
-            ]
-            
+          Container(
+            height: double.infinity,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                colors: [
+                  Color.fromRGBO(40, 0, 81, 1),
+                  Color.fromRGBO(115, 51, 100, 1),
+                  Color.fromRGBO(115, 51, 100, 1),
+                ],
+              ),
+            ),
+            child: Column(
+              children: [
+                Container(
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 80),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Hello,',
+                          style: GoogleFonts.breeSerif(
+                            fontWeight: FontWeight.w700,
+                            color: Colors.white,
+                            fontSize: 18,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        Text(
+                          '${ap.userModel.firstname} ${ap.userModel.lastname}',
+                          style: GoogleFonts.breeSerif(
+                            fontWeight: FontWeight.w700,
+                            color: Colors.white,
+                            fontSize: 22,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
           
-        ),
-        child: Column(
-          children: [
-           /*  Container(
-              child: Padding (
-                padding: EdgeInsets.only(top: 80),
-                child: Text("Hello ${ap.userModel.firstname} ${ap.userModel.lastname}" , style: GoogleFonts.montserrat(
-                  textStyle: TextStyle(
-                    fontSize: 17,
-                    color: Colors.white,
-                    fontWeight: FontWeight.normal,
-                  ),
-                )), ),
-            ), */
-             Container(
-              child: Padding (
-                padding: EdgeInsets.only(top: 80),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Hello,',
-                      style: GoogleFonts.breeSerif(
-                        fontWeight: FontWeight.w700,
-                        color: Colors.white,
-                        fontSize: 18,
-                       // decoration: TextDecoration.underline,
-                        //decorationColor: Color(0xFFBD1616),
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                    Text(
-                      '${ap.userModel.firstname} ${ap.userModel.lastname}',
-                      style: GoogleFonts.breeSerif(
-                        fontWeight: FontWeight.w700,
-                        color: Colors.white,
-                        fontSize: 22,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ],
-                ) ),
-            ),
-          ],
-        ),
-      ),
-       Padding(
+          Padding(
             padding: const EdgeInsets.only(top: 150.0),
             child: Container(
               decoration: const BoxDecoration(
                 borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(40),
-
-
-
-                  
                   topRight: Radius.circular(40),
                 ),
                 color: Colors.white,
               ),
               height: double.infinity,
               width: double.infinity,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 18.0, right: 18 ,top: 10),
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      Container(
-                        color: Colors.blue,
-                        height: 200,
-                        
-                        child: ListView.builder(
-                          scrollDirection: Axis.horizontal,
-                          //itemCount: 4,
-                          itemBuilder: (context, index) {
-                            return Container(
-                              width: 150,
-                              child : Row(
-                              //mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Stack(
-                                children: [
-                                  Container(
-                                    width: 80,
-                                    height: 80,
-                                    decoration: BoxDecoration(
-                                      shape: BoxShape.circle,
-                                      gradient: LinearGradient(
-                                        colors: [Colors.blue, Colors.green],
-                                        begin: Alignment.topLeft,
-                                        end: Alignment.bottomRight,
-                                      ),
-                                    ),
-                                  ),
-                                  Positioned.fill(
-                                    child: Align(
-                                      alignment: Alignment.center,
-                                      child: const Icon(
-                                        Icons.person,
-                                        color: Colors.white,
-                                        size: 30,
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
+              child: Column(
+                children: [
+                  /* Container(
+                    height: 200, // Set a fixed height for the ListView
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      shrinkWrap: true,
+                      children: [
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            CircleAvatarWithGradient(),
+                            CircleAvatarWithGradient(),
+                            CircleAvatarWithGradient(),
+                            CircleAvatarWithGradient(),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ), */
+                  SizedBox(height: 10),
+                  // Other widgets in the Column
+                  Stack(
+                  children: [
+                    Positioned(
+                      top: 20,
+                      left: 20,
+                      child: Container(
+                        width:width*0.8,
+                        height: height*0.8,
+                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
+                        //color: Colors.amber,
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        print(currentIndex);
+                      },
+                      child: CarouselSlider(
+                        items: imageList
+                            .map(
+                              (item) => Image.asset(
+                                item['image_path'],
+                                fit: BoxFit.cover,
+                                width: width*0.8,
                                 
-                                 ])
-                            );
+                              ),
+                            )
+                            .toList(),
+                        carouselController: carouselController,
+                        options: CarouselOptions(
+                          scrollPhysics: const BouncingScrollPhysics(),
+                          autoPlay: true,
+                          aspectRatio: 2,
+                          viewportFraction: 1,
+                          onPageChanged: (index, reason) {
+                            setState(() {
+                              currentIndex = index;
+                            });
                           },
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                    Positioned(
+                      bottom: 10,
+                      left: 0,
+                      right: 0,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: imageList.asMap().entries.map((entry) {
+                          return GestureDetector(
+                            onTap: () => carouselController.animateToPage(entry.key),
+                            child: Container(
+                              width: currentIndex == entry.key ? 17 : 7,
+                              height: 7.0,
+                              margin: const EdgeInsets.symmetric(
+                                horizontal: 3.0,
+                              ),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: currentIndex == entry.key
+                                    ? myColor1
+                                    : myColor1,
+                              ),
+                            ),
+                          );
+                        }).toList(),
+                      ),
+                    ),
+                  ],
                 ),
+                ],
               ),
             ),
           ),
+              
+          
+        ],
+      ),
+  );
+  }
+}
 
-      ],
-    )
-      
-      
-     
+class CircleAvatarWithGradient extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: CircleAvatar(
+        radius: 40,
+        child: Container(
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            gradient: LinearGradient(
+              colors: [
+                Color.fromRGBO(40, 0, 81, 1),
+                Color.fromRGBO(115, 51, 100, 1),
+                Color.fromRGBO(115, 51, 100, 1),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
